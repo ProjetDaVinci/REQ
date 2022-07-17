29e087f7fac8ac02c35703fe3e143948af47110a
Иван Мингалев <nero.fan69@gmail.com>
2022-07-18 05:25:14 +0600

    feat: auth and styles

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,10 +9,10 @@ import GithubProvider from "next-auth/providers/github";
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
